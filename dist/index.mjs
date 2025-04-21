44b227ed7fd061a37841db23ec30d447a5f370cd
Nishant Saurav <nsaurav@enlil.com>
2025-04-21 17:19:37 +0530

    ui library build

---
+++ dist/index.mjs
@@ -0,0 +1,6 @@
+// src/components/Button.tsx
+import { jsx } from "react/jsx-runtime";
+var Button = ({ children }) => /* @__PURE__ */ jsx("button", { className: "px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition", children });
+export {
+  Button
+};
