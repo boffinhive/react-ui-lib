a20a44db7f7c333e98a483204ff671c1b4ff0912
Nishant Saurav <nsaurav@enlil.com>
2025-04-21 19:15:58 +0530

    peer dependencies

--- src/components/StyledButton.tsx
+++ src/components/StyledButton.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Button, ButtonProps } from 'primereact/button';
-import '../styles/tailwind.css';
-import '../styles/primereact.scss';
+import React from "react";
+import { Button, ButtonProps } from "primereact/button";
+import "../styles/tailwind.css";
+import "../styles/primereact.scss";
 
 export const StyledButton = (props: ButtonProps) => {
-  return <Button {...props} className={`p-button ${props.className || ''}`} />;
+  return <Button {...props} className={`p-button ${props.className || ""}`} />;
 };
