79293fd5500c4e54622646d14fb39783f70bd82d
Nishant Saurav <nsaurav@enlil.com>
2025-04-22 15:25:21 +0530

    updated package and button component

--- dist/index.d.mts
+++ dist/index.d.mts
@@ -1,9 +1,9 @@
 import * as react_jsx_runtime from 'react/jsx-runtime';
+import { ButtonProps } from 'primereact/button';
 
-type Props = {
-    label: string;
-    onClick?: () => void;
-};
-declare const Button: ({ label, onClick }: Props) => react_jsx_runtime.JSX.Element;
+interface CustomButtonProps extends ButtonProps {
+    className?: string;
+}
+declare const Button: ({ className, ...props }: CustomButtonProps) => react_jsx_runtime.JSX.Element;
 
-export { Button };
+export { Button, type CustomButtonProps };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,18 @@
-import { Button as PrimeButton } from "primereact/button";
+import { Button as PrimeButton, ButtonProps } from "primereact/button";
+import clsx from "clsx";
 
-type Props = {
-  label: string;
-  onClick?: () => void;
-};
+export interface CustomButtonProps extends ButtonProps {
+  className?: string;
+}
 
-export const Button = ({ label, onClick }: Props) => {
+export const Button = ({ className, ...props }: CustomButtonProps) => {
   return (
     <PrimeButton
-      label={label}
-      onClick={onClick}
-      className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg"
+      {...props}
+      className={clsx(
+        "px-4 py-2 rounded text-white bg-blue-500 hover:bg-blue-700 font-medium",
+        className
+      )}
     />
   );
 };
