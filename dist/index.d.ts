82c74c7ee2b8515e733369c053d8ececc9d1bc88
Nishant Saurav <nsaurav@enlil.com>
2025-04-22 21:28:06 +0530

    reset

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-import { ButtonProps } from 'primereact/button';
-
-interface CustomButtonProps extends ButtonProps {
-    className?: string;
-}
-declare const Button: ({ className, ...props }: CustomButtonProps) => react_jsx_runtime.JSX.Element;
-
-export { Button, type CustomButtonProps };
